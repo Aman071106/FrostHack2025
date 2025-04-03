3a1311ccb06c8e7086c8b85aa109eb7facfec4ea
hustlewithkunal <mittalk237@gmail.com>
2025-04-03 16:15:29 +0000

    after removing envs

--- aibackend/agents/test.py
+++ aibackend/agents/test.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://127.0.0.1:5000/update_vector_store"  # Change this to your Flask API URL
-file_path = "/Users/harshyadav/Documents/Frosthack 2025/frosthack2025/FrostHack2025/aibackend/agents/user125.csv"
+file_path = "/home/venom/frosthack2025/FrostHack2025/aibackend/agents/user125.csv"
 
 files = {'file': open(file_path, 'rb')}  # 'file' must match request.files['file'] in Flask
 response = requests.post(url, files=files)
